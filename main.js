eddf262fefd4eab47352d5ba79684329b7cf7f9d
Sullivan Pierce <sullypierce@gmail.com>
2019-10-10 14:26:43 -0500

    initial push

---
+++ main.js
@@ -0,0 +1,9 @@
+for (i = 1; i <= 100; i++) {
+    if (i % 5 === 0) {
+        console.log("chicken");
+    } else if (i % 7 === 0) {
+        console.log("monkey");
+    } else {
+        console.log(i);
+    }
+}
